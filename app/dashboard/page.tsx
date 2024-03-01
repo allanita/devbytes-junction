35caa808cefeeb599eb6d17ebcd8bd9efea75be8
Ariel Llanita <ariel@eetech.com>
2024-03-01 09:56:51 +0800

    test commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,12 +7,10 @@ type Props = {};
 const Page = (props: Props) => {
   return (
     <div className="h-screen grid grid-cols-12 grid-rows-4">
-      {/* RIGHT */}
       <div className="col-span-12 lg:col-span-2 row-span-4 hidden lg:block bg-base-200 p-4">
         <SideNav />
       </div>
 
-      {/* CONTENT */}
       <div className="col-span-12 lg:col-span-10 row-span-4">
         <Content />
       </div>
